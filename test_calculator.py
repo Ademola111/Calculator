6589d8535588a37a378426d666197b3f94b38634
Ademola111 <onayemi.hakeem@gmail.com>
2023-05-12 22:57:50 +0100

    first commit

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+"""
+Unit tests for the calculator library
+"""
+import calculator
+
+class Test_calculator:
+    
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+    
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
