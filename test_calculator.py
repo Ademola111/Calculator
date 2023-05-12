b3add78aa8dcd07ebc20abab08ff417f2bb5f91a
Ademola111 <onayemi.hakeem@gmail.com>
2023-05-12 23:57:34 +0100

    flake8 --statistic --exclude=callab

--- test_calculator.py
+++ test_calculator.py
@@ -5,11 +5,8 @@ import calculator
 
 
 class Test_calculator:
-    
-    
     def test_addition(self):
-        assert 4 == calculator.add(2,2)
-    
-    
+        assert 4 == calculator.add(2, 2)
+
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4,2)
+        assert 2 == calculator.subtract(4, 2)
