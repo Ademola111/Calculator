3070912a2df37b00a5e36e17fb8be6dea8720086
Ademola111 <onayemi.hakeem@gmail.com>
2023-05-13 00:04:25 +0100

    flake8 --statistic --exclude=callab 2

--- test_calculator.py
+++ test_calculator.py
@@ -10,3 +10,6 @@ class Test_calculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
